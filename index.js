38fc461228a3f4a085ebcbe50bab0d85afbc3fdc
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-02 16:03:47 +0100

    Add some logging

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ const ROUTES = [
 ]
 
 function updateResponseContent(resp, filePath) {
+    console.log(`Reading content from ${filePath}`);
     fs.readFile(filePath, (err, data) => {
         if (err) {
             console.error(err);
             return;
         }
         resp.write(data);
+        console.log(`Sending response`);
         resp.end();
     });
 }
 
 const server = createServer((req, res) => {
+    console.log(`From ${req.url}`);
     const matchingRoute = ROUTES.find((route) => route.urlPath === req.url)
     // Check if a 404 should be sent back
     if (!matchingRoute) {
